c0a0d9c59785c6948d7636cdcce1cfd7bc0016b8
Wang Lin <maythusharmusic@gmail.com>
2025-12-05 16:37:34 +0630

    Update config.py

--- config.py
+++ config.py
@@ -61,10 +61,10 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL_LINK = getenv("SUPPORT_CHANNEL_LINK", "@everythingreset")
-SUPPORT_CHAT_LINK = getenv("SUPPORT_CHAT_LINK", "@sasukemusicsupportchat")
+SUPPORT_CHAT_LINK = getenv("SUPPORT_CHAT_LINK", "@ChitChatZone14")
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/everythingreset")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sasukemusicsupportchat")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+eAA0nwjFZZ0yYTg9")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
